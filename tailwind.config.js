5eefe43499218c3b148c81f5ce8266f34b0951ee
grcodeman <grcodeman@gmail.com>
2025-06-29 10:22:18 -0500

    adding fonts

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        charcoal: '#1A1A1D',
+        teal: '#00FFC3',
+        violet: '#B200FF',
+        offwhite: '#E5E5E5',
+      },
+      fontFamily: {
+        heading: ['JetBrains Mono', 'monospace'],
+        body: ['Inter', 'sans-serif'],
+      },
+      letterSpacing: {
+        widest: '.25em',
+      }
+    },
+  },
+  plugins: [],
+}
