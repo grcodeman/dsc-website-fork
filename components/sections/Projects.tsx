357abe2d29306c79fe071eb3efa37ca12e91751a
grcodeman <grcodeman@gmail.com>
2025-09-26 11:21:39 -0400

    removing unused import

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ProjectCard from '../cards/ProjectCard';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const Projects = () => {
   // Sample project data (empty placeholders as requested)
